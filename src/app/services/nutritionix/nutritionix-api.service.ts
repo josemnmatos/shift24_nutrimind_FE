b367d46d955ac0bcf7216d7fa3ce6f92244a14e1
josemnmatos <josemnmatos@gmail.com>
2024-04-20 02:31:53 +0100

    Nutritionix data

--- src/app/services/nutritionix/nutritionix-api.service.ts
+++ src/app/services/nutritionix/nutritionix-api.service.ts
@@ -1,8 +1,29 @@
 import { Injectable } from '@angular/core';
+import { environment } from '../../../environments/environment';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root',
 })
 export class NutritionixApiService {
-  constructor() {}
+  private apiUrl = 'https://trackapi.nutritionix.com/v2';
+  private nplServiceUrl = this.apiUrl + '/natural/nutrients';
+  private NUTRITIONIX_APP_ID = environment.NUTRITIONIX_APP_ID;
+  private NUTRITIONIX_API_KEY = environment.NUTRITIONIX_API_KEY;
+
+  private headers = {
+    'x-app-id': this.NUTRITIONIX_APP_ID,
+    'x-app-key': this.NUTRITIONIX_API_KEY,
+    'Content-Type': 'application/json',
+  };
+
+  constructor(private http: HttpClient) {}
+
+  public getNutritionixData(input: string) {
+    return this.http.post(
+      this.nplServiceUrl,
+      { query: input, timezone: 'Europe/London' },
+      { headers: this.headers }
+    );
+  }
 }
