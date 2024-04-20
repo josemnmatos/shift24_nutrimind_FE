cb68735e8f3b9f7c8d8202c1872bde77a5feeeba
joaodunas <joaomariadonato@gmail.com>
2024-04-20 03:16:43 +0100

    Websocket

--- src/app/services/websocket/websocket.service.ts
+++ src/app/services/websocket/websocket.service.ts
@@ -14,16 +14,20 @@ export class WebsocketService {
   public message$: BehaviorSubject<string> = new BehaviorSubject('');
   constructor() {}
 
-  socket = io('http://localhost:3000');
+  socket = io('http://localhost:5000');
 
   public sendMessage(message: any) {
     console.log('sendMessage: ', message);
-    this.socket.emit('message', message);
+    this.socket.emit('new_message', {
+      conversationId: '0',
+      content: message,
+    });
   }
 
   public getNewMessage = () => {
-    this.socket.on('message', (message) => {
+    this.socket.on('new_message_r', (message) => {
       this.message$.next(message);
+      console.log('getNewMessage: ', message);
     });
 
     return this.message$.asObservable();
