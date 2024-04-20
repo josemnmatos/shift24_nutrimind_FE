750cbd365971eff9c41ae89ba960eaeb88c1a863
josemnmatos <josemnmatos@gmail.com>
2024-04-20 22:55:01 +0100

    fix

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,6 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-import { HistoryComponent } from './history/history.component';
+//import { HistoryComponent } from './history/history.component';
 import { ChatbotComponent } from './chatbot/chatbot.component';
 import { AuthenticationComponent } from './authentication/authentication.component';
 
@@ -8,7 +8,7 @@ export const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' }, // default route
   //{ path: '**', redirectTo: 'home' }, // if route doesn't exist then redirect to home
   { path: 'home', component: HomeComponent },
-  { path: 'history', component: HistoryComponent },
+  // { path: 'history', component: HistoryComponent },
   { path: 'chatbot', component: ChatbotComponent },
   {
     path: 'auth',
