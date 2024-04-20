8a8af1caf26a991b25ecb9b8760a9ea19e20d343
josemnmatos <josemnmatos@gmail.com>
2024-04-20 18:15:53 +0100

    calendar

--- src/app/history/history.component.ts
+++ src/app/history/history.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CalendarModule } from 'primeng/calendar';
+import { faCalendar } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-history',
   standalone: true,
-  imports: [],
+  imports: [CalendarModule, FormsModule],
   templateUrl: './history.component.html',
-  styleUrl: './history.component.scss'
+  styleUrl: './history.component.scss',
 })
 export class HistoryComponent {
+  date: Date;
+  faCalendar = faCalendar;
 
+  constructor() {
+    this.date = new Date();
+  }
 }
