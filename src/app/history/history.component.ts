20def3073759000f438230348304acb89d067b0e
Valentim Garcia <vldg10sd@gmail.com>
2024-04-20 19:40:11 +0100

    history date request

--- src/app/history/history.component.ts
+++ src/app/history/history.component.ts
@@ -1,12 +1,36 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {CalendarModule} from 'primeng/calendar';
+import {DataRequestsService, URL_DEFAULT} from "../services/requests/data-requests.service";
 
 @Component({
   selector: 'app-history',
   standalone: true,
-  imports: [],
+  imports: [CalendarModule, FormsModule],
   templateUrl: './history.component.html',
-  styleUrl: './history.component.scss'
+  styleUrl: './history.component.scss',
 })
-export class HistoryComponent {
+export class HistoryComponent implements OnInit {
+  date?: Date[];
 
+  constructor(private dataRequests: DataRequestsService) { }
+
+  ngOnInit(): void {
+
+  }
+
+  public onClose(): void {
+    if (this.date) {
+      this.dataRequests.sendRangedDateMeals(URL_DEFAULT + '/api/get_meals_filtered/', {
+        startDate: this.getConvertedDate(this.date[0]),
+        endDate: this.getConvertedDate(this.date[1])
+      }).subscribe((response) => {
+        console.log(response);
+      });
+    }
+  }
+
+  private getConvertedDate(date: Date): string {
+    return date.getFullYear() + '-' + (date.getMonth() + 1) + '-' + date.getDate();
+  }
 }
---
+++ src/app/services/requests/data-requests.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+export const URL_DEFAULT = 'http://localhost:8000';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataRequestsService {
+
+  constructor(private http: HttpClient) { }
+
+  // get example
+  public getData(url: string): Observable<any> {
+    return this.http.get(url);
+  }
+
+  public sendRangedDateMeals(url: string, data: any): Observable<any> {
+    return this.http.post(url, data);
+  }
+}
