e53638030003236eb7a14c96538b04f830e36d6e
josemnmatos <josemnmatos@gmail.com>
2024-04-21 11:28:12 +0100

    update

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: false,
   NUTRITIONIX_APP_ID: '8a1ad28b',
   NUTRITIONIX_API_KEY: '342b406b0b0bdb4b76d3585032e3404f',
-  API_ENDPOINT: 'https://64.227.122.94:8000',
+  API_ENDPOINT: 'https://64.227.122.94',
 };
